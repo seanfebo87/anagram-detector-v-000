4dd0343065a1165e69004f60e65ba9fe514bf118
Sean Febo <seanfebo@techie.com>
2018-04-12 19:54:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end 
+
+def match(array)
+  array.each do |w|
+    if w.length == @word.length
+      w 
+    else
+      []
+    end
+  end
+end
+binding.pry
+end
