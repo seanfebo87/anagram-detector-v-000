c4afabc044ab9ce4effa5d20b2a2d1a3edb9c89d
Sean Febo <seanfebo@techie.com>
2018-04-12 20:31:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
 def match(array)
   word_split = @word.split("")
   array.each do |w|
-   w_split = #{w}.split("")
+   w_split = w.split("")
     if word_split.sort == w_split
       w_split
     else
